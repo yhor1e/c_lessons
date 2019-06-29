1e61f91e94f1a0ce259ecb03f0ad98661e43dad8
yhorie <yuk.horie@gmail.com>
2019-06-30 07:41:49 +0900

    finish lesson 19

--- src/hello.c
+++ src/hello.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
+
+void f(long a){
+  a += 100;
+  printf("%ld\n", a);
+}
+
 int main(void){
 
-  char s1[] = {'a', 'b', 'c', '\0'};
-  char s2[] = "abc";
+  long a = 1000;
+  f(a);
 
-  printf("%c\n", s1[1]);
-  printf("%c\n", s2[1]);
 
   return 0;
 }
