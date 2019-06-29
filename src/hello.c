a1537388bcf08f62e316190ac01622d1052a2fd8
yhorie <yuk.horie@gmail.com>
2019-06-30 07:45:31 +0900

    finish lesson 20

--- src/hello.c
+++ src/hello.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 
-void f(long a){
-  a += 100;
-  printf("%ld\n", a);
+void f(long *pa){
+  *pa += 100;
+  printf("%ld\n", *pa);
 }
 
 int main(void){
 
   long a = 1000;
-  f(a);
+  f(&a);
 
 
   return 0;
