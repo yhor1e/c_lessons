fd3b50886040a889fe5db884bba55c9eb67ae560
yhorie <yuk.horie@gmail.com>
2019-06-30 07:06:33 +0900

    finish lesson 14

--- src/hello.c
+++ src/hello.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 
-float getMax (float a, float b);
-
-void f(void) {
-  static int a = 0;
-  a++;
-  printf("a:%d\n", a);
-}
-
-
 int main(void){
 
-  f();
-  f();
-  f();
+  int sales[3];
+  sales[0] = 200;
+  sales[1] = 400;
+  sales[2] = 500;
+
+  printf("%d\n", sales[1]);
+
+  int sales2[] = {200, 400, 500};
+
+  printf("%d\n", sales2[1]);
 
   return 0;
 }
