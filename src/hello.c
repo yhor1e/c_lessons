53f287d305df87fc45c47ebff840266672089b3c
yhorie <yuk.horie@gmail.com>
2019-06-30 06:57:54 +0900

    finish lesson 11

--- src/hello.c
+++ src/hello.c
@@ -2,26 +2,15 @@
 
 float getMax (float a, float b);
 
-
-void sayHi(void) {
-  printf("hi!\n");
-}
-
 int main(void){
 
   float result;
   result = getMax(2.3, 5.2);
   printf("%f\n", result);
 
-  sayHi();
-
   return 0;
 }
 
 float getMax (float a, float b){
-  if(a >= b) {
-    return a;
-  } else {
-    return b;
-  }
+  return (a >= b) ? a: b;
 }
